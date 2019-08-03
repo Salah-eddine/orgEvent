3cc09d93caceba3b62a59a482b07a71831f779d9
isma <isma@darkvador.localdomain>
2019-08-03 19:33:35 +0200

    installation de laravel + adminlte et modification de l'inscription

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index(User $user )
+    {
+      //  $users= User::where('email', $user)->first();
+     if(Auth::user()->type == "Service") {
+         return view('home');
+     }elseif(Auth::user()->type == "Demande"){
+         return view('service');
+     }
+
+     //  dd($users);
+
+
+    }
+
+    }
+
